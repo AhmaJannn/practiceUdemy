7471e75ee35ef4f12f5e1d2b1d641d664ca977a5
Ahmed <Mukhsinzade@mail.ru>
2021-12-30 02:20:29 +0300

    Object

--- script.js
+++ script.js
@@ -1,27 +1,44 @@
 'use strict';
 
-function first() {
-	//Do something
-	setTimeout(function () {
-		console.log(1);
-	}, 500);
+const options = {
+	name: 'test',
+	width: 1024,
+	height: 1024,
+	colors: {
+		border: 'black',
+		bg: 'red',
+	},
+	makeTest: function () {
+		console.log('test');
+	}
+};
+
+const { border, bg } = options.colors;
+
+console.log(border);
+
+options.makeTest();
+
+console.log(Object.keys(options).length);
+
+
+//console.log(options["colors"]["border"]);
+
+//delete options.name;
+
+//console.log(options);
+
+
+let count = 0;
+for (let key in options) {
+	//if (typeof (options[key]) === 'object') {
+	//	for (let i in options[key]) {
+	//		console.log(`Свойство: ${i} имеет значение ${options[key][i]}`);
+	//	}
+	//} else {
+	//	console.log(`Свойство: ${key} имеет значение ${options[key]}`);
+	//}
+	count++;
 }
 
-function second() {
-	console.log(2);
-}
-
-first();
-second();
-
-function learnJS(lang, callback) {
-	console.log(`Я учу: ${lang}`);
-	callback();
-}
-
-function done() {
-	console.log('Я прошел этот урок!');
-}
-
-learnJS('JavaScript', done);
-
+console.log(count);
