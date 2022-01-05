846e2743da252b0fcd1d2014349ebce388b57b6b
Ahmed <Mukhsinzade@mail.ru>
2022-01-05 22:19:32 +0300

    Dynamic typing

--- script.js
+++ script.js
@@ -1,23 +1,51 @@
 'use strict';
 
-function Hello() {
-	console.log("Hello, world!");
-	debugger;
+//To String 
+
+// 1)
+console.log(typeof (String(null)));
+console.log(typeof (String(5)));
+
+// 2) 
+console.log(typeof (5 + ''));
+
+const num = 5;
+console.log("https://vk.com/catalog/" + num);
+
+const fontSize = 26 + 'px';
+
+//To Number
+
+// 1)
+console.log(typeof (Number('5')));
+
+// 2)
+console.log(typeof (+'5'));
+
+// 3)
+console.log(typeof (parseInt("15px", 10)));
+
+let answ = +prompt("Hello", '');
+
+//To Boolean
+
+//0, null, undefined, '', NaN = false
+
+// 1)
+let switcher = null;
+
+if (switcher) {
+	console.log('Working...');
 }
 
-Hello();
+switcher = 1;
 
-function hi() {
-	console.log("Say hi!");
+if (switcher) {
+	console.log('Working...');
 }
 
-hi();
+// 2) 
+console.log(typeof (Boolean('5')));
 
-const arr = [1, 62, 13, 84, 124],
-	sorted = arr.sort(compareNum);
-
-function compareNum(a, b) {
-	return a - b;
-}
-
-console.log(sorted);
+// 3
+console.log(typeof (!!"55555")); 
