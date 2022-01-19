c40aa4a6cf6f7d56a97e04b74efa7b609a07795b
Ahmed <Mukhsinzade@mail.ru>
2022-01-19 19:22:36 +0300

    Async and defer (script loaded)

--- js/script.js
+++ js/script.js
@@ -1,36 +1,15 @@
-// touchstart
-// touchmove
-// touchend
-// touchenter
-// touchleave
-// tocuchcancel
+"use strict";
+
+const p = document.querySelectorAll('p');
+console.log(p);
 
 
+function loadScript(src) {
+	const script = document.createElement('script');
+	script.src = src;
+	script.async = false;
+	document.body.append(script);
+}
 
-document.addEventListener('DOMContentLoaded', () => {
-	const box = document.querySelector('.box');
-
-	box.addEventListener('touchstart', (event) => {
-		event.preventDefault();
-
-		console.log('start');
-		//console.log(event.changedTouches);
-	});
-
-	box.addEventListener('touchmove', (event) => {
-		event.preventDefault();
-
-		//console.log('move');
-		console.log(event.targetTouches[0].pageX);
-	});
-
-	//box.addEventListener('touchend', (event) => {
-	//	event.preventDefault();
-
-	//	console.log('end');
-	//});
-});
-
-// touches 
-// targetTouches 
-// changedToches 
+loadScript('js/test.js');
+loadScript('js/some.js');
