44aa36a9a0c9beab9f0877d6d5a13feb2932966c
Ahmed <Mukhsinzade@mail.ru>
2022-01-26 06:25:59 +0300

    Delegation events and ClassList

--- js/script.js
+++ js/script.js
@@ -1,15 +1,42 @@
 "use strict";
 
-const p = document.querySelectorAll('p');
-console.log(p);
+const wrapper = document.querySelector('.btn-block'),
+	btns = wrapper.querySelectorAll('button');
 
+//console.log(btns[0].classList.length);
+//console.log(btns[0].classList.item(1));
+//console.log(btns[0].classList.add('red'));
+//console.log(btns[0].classList.remove('blue'));
+//console.log(btns[0].classList.toggle('blue'));
 
-function loadScript(src) {
-	const script = document.createElement('script');
-	script.src = src;
-	script.async = false;
-	document.body.append(script);
-}
+//if (btns[0].classList.contains('red')) {
+//	console.log('red');
+//}
 
-loadScript('js/test.js');
-loadScript('js/some.js');
+btns[0].addEventListener('click', () => {
+	//if (btns[1].classList.contains('red')) {
+	//	btns[1].classList.remove('red');
+	//} else {
+	//	btns[1].classList.add('red');
+	//}
+
+	btns[1].classList.toggle('red');
+});
+
+//console.log(btns[0].className);
+
+wrapper.addEventListener('click', event => {
+	if (event.target && event.target.matches("button.red")/*event.target.tagName == "BUTTON"*/ /*event.target.classList.contains('blue')*/) {
+		console.log("Hello");
+	}
+});
+
+//btns.forEach(btn => {
+//	btn.addEventListener('click', () => {
+//		console.log("Hello");
+//	});
+//});
+
+const btn = document.createElement('button');
+btn.classList.add('red');
+wrapper.append(btn);
