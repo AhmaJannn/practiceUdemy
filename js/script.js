9814a6fd08b12c61d40690580992b8414fd5a808
Ahmed <Mukhsinzade@mail.ru>
2022-02-07 13:04:53 +0300

    Update shablon (add rest parametr)

--- js/script.js
+++ js/script.js
@@ -159,12 +159,13 @@ window.addEventListener('DOMContentLoaded', () => {
 	// Sample 
 
 	class MenuCard {
-		constructor(scr, alt, title, descr, price, parentSelector) {
+		constructor(scr, alt, title, descr, price, parentSelector, ...classes) {
 			this.scr = scr;
 			this.alt = alt;
 			this.title = title;
 			this.descr = descr;
 			this.price = price;
+			this.classes = classes;
 			this.parent = document.querySelector(parentSelector);
 			this.transfer = 27;
 			this.changeToUAH();
@@ -176,8 +177,14 @@ window.addEventListener('DOMContentLoaded', () => {
 
 		render() {
 			const element = document.createElement('div');
+
+			if (this.classes.length === 0) {
+				this.classes = 'menu__item';
+				element.classList.add(this.classes);
+			} else {
+				this.classes.forEach(className => element.classList.add(className));
+			}
 			element.innerHTML = `
-			<div class="menu__item">
 				<img src=${this.scr} alt=${this.alt}>
 				<h3 class="menu__item-subtitle">${this.title}</h3>
 				<div class="menu__item-descr">${this.descr}</div>
@@ -186,7 +193,6 @@ window.addEventListener('DOMContentLoaded', () => {
 					<div class="menu__item-cost">Цена:</div>
 					<div class="menu__item-total"><span>${this.price}</span> грн/день</div>
 				</div>
-			</div>
 			`;
 			this.parent.append(element);
 		}
@@ -198,7 +204,7 @@ window.addEventListener('DOMContentLoaded', () => {
 		'Меню "Фитнес"',
 		'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
 		9,
-		'.menu .container'
+		'.menu .container',
 	).render();
 
 	new MenuCard(
@@ -207,7 +213,9 @@ window.addEventListener('DOMContentLoaded', () => {
 		'Меню "Премиум"',
 		'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
 		20,
-		'.menu .container'
+		'.menu .container',
+		'menu__item',
+		'big'
 	).render();
 
 	new MenuCard(
@@ -216,7 +224,8 @@ window.addEventListener('DOMContentLoaded', () => {
 		'Меню "Постное"',
 		'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
 		15,
-		'.menu .container'
+		'.menu .container',
+		'menu__item'
 	).render();
 
 });
