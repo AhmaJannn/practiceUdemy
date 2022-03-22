51a67e47bee452edf193c0d6bcc588e1befb4a42
AhmaJannn <Mukhsin-zade@MAIL.RU>
2022-03-22 16:16:17 +0300

    Reg Exp function add

--- js/script.js
+++ js/script.js
@@ -56,6 +56,10 @@ window.addEventListener('DOMContentLoaded', () => {
 	let slideIndex = 1,
 		offset = 0;
 
+	function takeOnlyNum(str) {
+		return +str.replace(/\D/g, "");
+	}
+
 	if (slides.length < 10) {
 		total.textContent = `0${slides.length}`;
 		current.textContent = `0${slideIndex}`;
@@ -94,10 +98,10 @@ window.addEventListener('DOMContentLoaded', () => {
 	}
 
 	next.addEventListener('click', () => {
-		if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+		if (offset == takeOnlyNum(width) * (slides.length - 1)) {
 			offset = 0;
 		} else {
-			offset += +width.slice(0, width.length - 2);
+			offset += takeOnlyNum(width);
 		}
 
 		slidesField.style.transform = `translateX(-${offset}px)`;
@@ -123,9 +127,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	prev.addEventListener('click', () => {
 		if (offset == 0) {
-			offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+			offset = takeOnlyNum(width) * (slides.length - 1);
 		} else {
-			offset -= +width.slice(0, width.length - 2);
+			offset -= takeOnlyNum(width);
 		}
 
 		slidesField.style.transform = `translateX(-${offset}px)`;
@@ -154,7 +158,7 @@ window.addEventListener('DOMContentLoaded', () => {
 			const slideTo = event.target.getAttribute('data-slide-to');
 
 			slideIndex = slideTo;
-			offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+			offset = takeOnlyNum(width) * (slideTo - 1);
 			slidesField.style.transform = `translateX(-${offset}px)`;
 
 			if (slideIndex < 10) {
