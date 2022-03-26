dc287d3426de3ead6bfa299caa02c5404d5f1e0d
AhmaJannn <Mukhsin-zade@MAIL.RU>
2022-03-26 18:41:07 +0300

    Getter/Setter

--- script.js
+++ script.js
@@ -1,44 +1,17 @@
 'use strict';
 
-//new RegExp('pattern', 'flags');
-// /pattern/f
+const persone = {
+	name: 'Alex',
+	age: 25,
 
-//const ans = prompt('Введите имя', '');
-//const ans = prompt('Введите числа', '');
+	get userAge() {
+		return this.age;
+	},
 
-//const reg = /n/ig;
-//const reg = /\d/g;
-//console.log(reg.test(ans));
-//console.log(ans.match(reg));
+	set userAge(num) {
+		this.age = num;
+	}
+};
 
-const str = 'My name is R2D2';
-
-//console.log(str.match(/\w\d\w\d/i));
-console.log(str.match(/\S/ig));
-
-/* Экранирование */
-
-// \d - цифры
-// \w - буквы
-// \s - пробелы
-
-// \D - НЕ цифры
-// \W - НЕ буквы
-// \S - НЕ пробелы
-
-/* Флаги */
-//i - Найти в независимости от регистра
-//g - (глобал) найти несколько вхождений
-//m - включает многострочный режим
-
-
-//console.log(ans.search(reg));
-//console.log(ans.match(reg));
-
-//const pass = prompt('Password', '');
-
-//console.log(pass.replace(/\./g, "*"));
-
-//console.log('12-34-56'.replace(/-/g, ":"));
-
-
+console.log(persone.userAge = 30);
+console.log(persone.userAge);
