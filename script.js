51dce8a0f11540487e0056eda0a5fb92653b1409
AhmaJannn <Mukhsin-zade@MAIL.RU>
2022-03-27 15:25:52 +0300

    Encapsulation

--- script.js
+++ script.js
@@ -1,17 +1,68 @@
 'use strict';
 
-const persone = {
-	name: 'Alex',
-	age: 25,
+//function User(name, age) {
+//	this.name = name;
+//	let userAge = age;
+//	//this.age = age;
 
-	get userAge() {
-		return this.age;
-	},
+//	this.say = function () {
+//		console.log(`Имя пользователя: ${this.name}, возраст: ${userAge}`);
+//	};
 
-	set userAge(num) {
-		this.age = num;
+//	this.getAge = function () {
+//		return userAge;
+//	};
+
+//	this.setAge = function (age) {
+//		if (typeof age === 'number' && age > 0 && age < 110) {
+//			userAge = age;
+//		} else {
+//			console.log("Недопустимое значение!");
+//		}
+//	};
+//}
+
+//const ivan = new User('Ivan', 35);
+
+//console.log(ivan.name);
+//console.log(ivan.getAge());
+
+//ivan.setAge(27);
+//ivan.name = 'Alex';
+
+//ivan.say(); 
+
+class User {
+	constructor(name, age) {
+		this.name = name;
+		this._age = age;
 	}
-};
 
-console.log(persone.userAge = 30);
-console.log(persone.userAge);
+	#surname = 'Mukhsin-Zade';
+
+	say = () => {
+		console.log(`Имя пользователя: ${this.name} ${this.#surname}, возраст: ${this._age}`);
+	}
+
+	get age() {
+		return this._age;
+	}
+
+	set age(age) {
+		if (typeof age === 'number' && age > 0 && age < 110) {
+			this._age = age;
+		} else {
+			console.log("Недопустимое значение!");
+		}
+	}
+}
+
+const akhmed = new User('Akhmed', 21);
+
+//console.log(akhmed.age);
+//akhmed.age = 99;
+//console.log(akhmed.age);
+
+console.log(akhmed.surname);
+
+akhmed.say();
