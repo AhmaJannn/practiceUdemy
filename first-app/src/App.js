2ea6163eaa82395cda3739930cde096cd1ddf80a
Akhmed Mukhsin-Zade <79843445+AhmaJannn@users.noreply.github.com>
2022-04-15 23:22:30 +0300

    Components and Elements

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,17 +1,58 @@
-import logo from './logo.svg';
+import {Component} from 'react';
 import './App.css';
+
+const Header = () => {
+	return <h2>Hello, world!</h2>
+}
+
+//const Field = () => {
+//	const holder = 'Enter here';
+//	const styleField = {
+//		width: '300px',
+//	};
+//	return <input 
+//				placeholder={holder} 
+//				type="text" 
+//				style={styleField}
+//			/>
+//}
+
+class Field extends Component {
+	render() {
+		const holder = 'Enter here';
+		const styleField = {
+			width: '300px',
+		};
+		return <input 
+					placeholder={holder} 
+					type="text" 
+					style={styleField}
+				/>
+	}
+}
+
+function Btn() {
+	const text = 'Log in';
+	//const res = () => {
+	//	return 'Log in';
+	//};
+	const logged = true;
+	//const p = <p>Log in</p>
+	return <button>{logged ? 'Enter' : text}</button>
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+		 <Header/>
+		 <Field/>
+		 <Btn/>
+		 <h1>Hello world what u doing frined</h1>
+		 <h2>Hello world what u doing  in small letters</h2>
+		 <h2>Hello world what u doing  in small letters</h2>
     </div>
   );
 }
 
+export {Header};
 export default App;
--- first-app/src/index.js
+++ first-app/src/index.js
@@ -1,35 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-//import App from './App';
-
-const text = "Hello, world!!!";
-
-//const elem = <h1>Hello, world!</h1>;
-const elem = (
-	<div>
-		<h2 className='text'>Текст:{text}</h2>
-		<label>
-			<input type="text" />
-			<button tabIndex="0" >Click me</button>
-		</label>
-		{/*<button />*/}
-	</div>
-);
-
-//const elem = React.createElement('h2', {className: 'header__text'}, 'Hello, world!');
-
-//const elem = {
-//	type: 'h2',
-//	props: {
-//		className: 'header__text',
-//		children: 'Hello, world!'
-//	}
-//};
+import App from './App';
+import { Header } from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	elem
+	<div className='header'>
+		<App/>
+			<Header/>
+	</div>
 );
 
 
