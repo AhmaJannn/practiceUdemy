c706b2b7921fdbad3eb0d4cbb99429191dbb5c12
Akhmed Mukhsin-Zade <79843445+AhmaJannn@users.noreply.github.com>
2022-05-04 14:37:27 +0300

    function generators

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
-import $ from 'jquery';
+'use strict';
 
-$(document).ready(function () {
-    $('.list-item:first').hover(function () {
-        $(this).toggleClass('active');
-    });
+// function* generator() {
+//     yield 'S';
+//     yield 'c';
+//     yield 'r';
+//     yield 'i';
+//     yield 'p';
+//     yield 't';
+// }
+//
+// const str = generator();
+//
+// console.log(str.next().value);
+// console.log(str.next());
+// console.log(str.next());
+// console.log(str.next());
+// console.log(str.next());
+// console.log(str.next());
+// console.log(str.next());
 
-    $('.list-item:eq(2)').on('click', function () {
-        $('.image:even').fadeToggle('slow');
-    });
+function* count(n) {
+    for (let i = 0; i < n; i++){
+        yield i;
+    }
+}
 
-    $('.list-item:eq(4)').on('click', function () {
-        $('.image:odd').animate({
-            opacity: 'toggle',
-            height: 'toggle'
-        }, 2000);
-    });
-});
+for (let k of count(7)){
+    console.log(k);
+}
+
+// const counter = count(7);
+//
+// console.log(counter.next().value);
+// console.log(counter.next().value);
+// console.log(counter.next().value);
+
