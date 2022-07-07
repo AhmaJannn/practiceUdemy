d39179870f0ca23971c62183ee45c555211456b6
Akhmed Mukhsin-Zade <79843445+AhmaJannn@users.noreply.github.com>
2022-07-07 11:02:00 +0300

    Bug Fixing, Component Lifecycle (Start)

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,51 +1,75 @@
-import './charList.scss';
-import abyss from '../../resources/img/abyss.jpg';
+import {Component} from "react";
+import MarvelService from "../../services/MarvelService";
+import Spinner from "../spinner/Spinner";
+import ErrorMessage from "../errorMessage/ErrorMessage";
 
-const CharList = () => {
+import './charList.scss';
+
+class CharList extends Component {
+
+    state = {
+        charList: [],
+        loading: true,
+        error: false
+    }
+
+    marvelService = new MarvelService();
+
+    componentDidMount() {
+        this.marvelService
+            .getAllCharacter()
+            .then(this.onCharListLoaded)
+            .catch(this.onError)
+    }
+
+
+    onCharListLoaded = (charList) => {
+        this.setState({charList, loading: false})
+    }
+
+    onError = () => {
+        this.setState({error: true, loading: false})
+    }
+
+
+    render() {
+        const {charList, loading, error} = this.state;
+        const errorMessage = error ? <ErrorMessage /> : null;
+        const spinner = loading ?  <Spinner /> : null;
+        const content = !(loading || error) ? charList.map((item) => {
+            const {id, name, thumbnail} = item;
+            return <CharListItem key={id} name={name} thumbnail={thumbnail} />
+        }) : null;
+
+
+        return (
+            <div className="char__list">
+                {errorMessage}
+                {spinner}
+                <ul className="char__grid">
+                {content}
+                </ul>
+                <button className="button button__main button__long">
+                    <div className="inner">load more</div>
+                </button>
+            </div>
+        )
+    }
+
+}
+
+const CharListItem = ({name, thumbnail}) => {
+
+    let imgStyle = {'objectFit' : 'cover'};
+    if ((thumbnail.lastIndexOf("image_not_available") > 0)) {
+        imgStyle = {'objectFit' : 'unset'};
+    }
+
     return (
-        <div className="char__list">
-            <ul className="char__grid">
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item char__item_selected">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-            </ul>
-            <button className="button button__main button__long">
-                <div className="inner">load more</div>
-            </button>
-        </div>
+            <li className="char__item">
+                <img src={thumbnail} alt={name} style={imgStyle}/>
+                <div className="char__name">{name}</div>
+            </li>
     )
 }
 
--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,18 +1,13 @@
 import {Component} from "react";
-
-import './randomChar.scss';
-import mjolnir from '../../resources/img/mjolnir.png';
 import MarvelService from "../../services/MarvelService";
 import Spinner from "../spinner/Spinner";
 import ErrorMessage from "../errorMessage/ErrorMessage";
 
-class RandomChar extends Component {
-    constructor(props) {
-        super(props);
-        this.updateChar()
-    }
+import mjolnir from '../../resources/img/mjolnir.png';
 
-    marvelService = new MarvelService();
+import './randomChar.scss';
+
+class RandomChar extends Component {
 
     state = {
         char: {},
@@ -20,8 +15,27 @@ class RandomChar extends Component {
         error: false
     }
 
+    marvelService = new MarvelService();
+
+    componentDidMount() {
+        this.updateChar()
+        // this.timerId = setInterval(() => {
+        //     this.updateChar()
+        // }, 15000)
+    }
+
+    componentWillUnmount() {
+        // clearInterval(this.timerId);
+    }
+
     onCharLoaded = (char) => {
         this.setState({char, loading: false})
+    }
+
+    onCharLoading = () => {
+        this.setState({
+            loading: true
+        })
     }
 
     onError = () => {
@@ -31,6 +45,7 @@ class RandomChar extends Component {
 
     updateChar = () => {
         const id = Math.floor(Math.random() * (1011196 - 1011027) + 1011027);
+        this.onCharLoading();
         this.marvelService
             .getCharacter(id)
             .then(this.onCharLoaded)
@@ -56,7 +71,7 @@ class RandomChar extends Component {
                     <p className="randomchar__title">
                         Or choose another one
                     </p>
-                    <button className="button button__main">
+                    <button className="button button__main" onClick={this.updateChar}>
                         <div className="inner">try it</div>
                     </button>
                     <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
@@ -69,9 +84,14 @@ class RandomChar extends Component {
 const View = ({char}) => {
     const { name, description, thumbnail, homepage, wiki } = char;
 
+    let imgStyle = {'objectFit' : 'cover'};
+    if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+        imgStyle = {'objectFit' : 'fill'};
+    }
+
     return (
         <div className="randomchar__block">
-            <img src={thumbnail} alt="Random character" className="randomchar__img"/>
+            <img className="randomchar__img" src={thumbnail} alt="Random character" style={imgStyle}/>
             <div className="randomchar__info">
                 <p className="randomchar__name">{name}</p>
                 <p className="randomchar__descr">
