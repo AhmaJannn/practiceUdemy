a1a6207af097049b89936eef3c084e78269aaa9a
Akhmed Mukhsin-Zade <79843445+AhmaJannn@users.noreply.github.com>
2022-07-19 17:59:54 +0300

    custom Hook

--- src/App.js
+++ src/App.js
@@ -1,34 +1,53 @@
-import { useRef, useState, useEffect } from "react";
+import { useState } from "react";
 import { Container } from "react-bootstrap";
 import "./App.css";
 
-const Form = () => {
-  // const [text, setText] = useState(null);
-  //
-  // const myRef = useRef(0);
-  //
-  // useEffect(() => {
-  //   myRef.current = text;
-  // });
+function useInputWithValidate(initialValue) {
+  const [value, setValue] = useState(initialValue);
 
-  const myRef = useRef(null);
-
-  const focusFirstTI = () => {
-    myRef.current.focus();
+  const onChange = (event) => {
+    setValue(event.target.value);
   };
+
+  const validateInput = (value) => {
+    return value.search(/\d/) >= 0;
+  };
+
+  const color = validateInput(value) ? "text-danger" : null;
+
+  return { value, onChange, validateInput, color };
+}
+
+const Form = () => {
+  // const [text, setText] = useState("");
+  // const [textArea, setTextArea] = useState("");
+  const text = useInputWithValidate("");
+  const textArea = useInputWithValidate("");
+
+  // const validateInput = (text) => {
+  //   return text.search(/\d/) >= 0;
+  // };
+
+  // const color = validateInput(text) ? "text-danger" : null;
+  // const color = text.validateInput() ? "text-danger" : null;
 
   return (
     <Container>
       <form className="w-50 border mt-5 p-3 m-auto">
         <div className="mb-3">
-          <label htmlFor="exampleFormControlInput1" className="form-label">
+          <input
+            className="form-control"
+            value={`${text.value} / ${textArea.value}`}
+            type="text"
+            readOnly
+          />
+          <label htmlFor="exampleFormControlInput1" className="form-label mt-3">
             Email address
           </label>
           <input
-            // onChange={(event) => setText(event.target.value)}
-            ref={myRef}
+            className={`form-control ${text.color}`}
+            onChange={text.onChange}
             type="email"
-            className="form-control"
             id="exampleFormControlInput1"
             placeholder="name@example.com"
           />
@@ -38,9 +57,8 @@ const Form = () => {
             Example textarea
           </label>
           <textarea
-            // value={myRef.current}
-            onClick={focusFirstTI}
-            className="form-control"
+            className={`form-control ${textArea.color}`}
+            onChange={textArea.onChange}
             id="exampleFormControlTextarea1"
             rows="3"
           ></textarea>
